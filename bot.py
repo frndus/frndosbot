7b12a0d0cc3257fed47f67ed98a762e6228959c5
brimar.orn@gmail.com <brimar.orn@gmail.com>
2018-10-05 02:44:21 +0000

    added a variety of spicy insults and a rare eybo generator

--- bot.py
+++ bot.py
@@ -6,13 +6,14 @@ import random
 import itertools
 import json
 import os
+import random
+from time import sleep
 from discord.utils import get
 
 
-#open file
-#TOKEN = get token from file
-#close file?
-with open("bot.json") as cfg:
+
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname("bot.json")))
+with open(__location__ + "\\bot.json") as cfg:
     config = json.load(cfg)
 
 token = config["token"]
@@ -22,6 +23,22 @@ prefix = config["command_prefix"]
 bot = commands.Bot(command_prefix=prefix)
 client = discord.Client()
 
+def getInsult():
+    insultList = [
+        "finger",
+        "fatso",
+        "diapyhead",
+        "fingro",
+        "diapy farty fingro brain idiot dumdum blaster",
+        "poopoopeepeeface",
+        "berny",
+        "pipper",
+        "ddosface",
+        "cunt",
+        "smoothbrain",
+        "ddos victim"
+    ]
+    return random.choice(insultList)
 
 def getEmojiByName(input):
     return get(bot.get_all_emojis(), name=input)
@@ -29,7 +46,6 @@ def getEmojiByName(input):
 def getCoolEmoji(ears=False):
     emoji_list = [
     "frndus",
-    "jew_basic",
     "viper",
     "fuck12",
     "scEybbus",
@@ -44,16 +60,28 @@ def getCoolEmoji(ears=False):
         return ":" + random.choice(emoji_list) + ":"
     else:
         return random.choice(emoji_list)
+
+def niceTry():
+    return "nice try, {}".format(getInsult())
     
 
- 
 @bot.event
 async def on_ready():
     print("I am running on: " + bot.user.name)
     print("with the id: " + bot.user.id)
-    #print("setting up...")
-    #print("finished")
+    sleep(0.5)
+    print("fastening diaper")
+    sleep(0.5)
+    print("got my rice dude")
+    sleep(0.5)
+    print("my power supply", end='')
+    for i in range(5):
+        print('.', end='', flush=True)
+        sleep(0.5)
+    sleep(0.5)
+    print()
     print("its gamer time my dude ")
+    sleep(0.5)
     print ("----------------------------------------------")
 
 @bot.command(pass_context=True)
@@ -90,11 +118,42 @@ async def bother(ctx, user: discord.Member):
     if(emoji):
         await bot.add_reaction(ctx.message, emoji)
     else:
-        print("ERROR EMOJI: " + rawEmoji + "NOT FOUND REEEEEEEEEEEEEEEE")
+        print("ERROR EMOJI: {} NOT FOUND REEEEEEEEEEEEEEEE".format(rawEmoji))
     message = random.choice(messages)
     message = ":fire:" + message + ":fire:"
     await bot.send_message(user, message)
-    await bot.say("bothered "  + user.name + " with the message: " + message)
+    await bot.say("bothered {} with the message: {}".format(user.name, message))
+
+
+@bot.command(pass_context=True)
+async def insult(ctx): 
+    await bot.say("{} you {}".format(ctx.message.author.mention, getInsult()))
+    
+@bot.command(pass_context=True)
+async def pizza(ctx):
+
+    message = ctx.message.content.split()
+    if len(message) != 2:
+        await bot.say(niceTry())
+        return
+    else:
+        message = message[-1]
+        if message == "0":
+            await bot.say("nice try idiot")
+            return
+        elif message.isdigit():
+            numpizza = random.randint(1, int(message))
+            await bot.say("how about a large number " + str(numpizza) + " with extra cheese you fat fuck")
+        else:
+            await bot.say("enter a number, {}".format(getInsult()))
+
+
+@bot.command(pass_context=True)
+async def rare(ctx):
+
+    dir =__location__ + "\\rare"
+
+    await bot.send_file(ctx.message.channel, dir + "\\" + random.choice(os.listdir(dir)))
 
 
 bot.run(token)
